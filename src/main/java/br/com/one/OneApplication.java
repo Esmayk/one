2b16726e3944e8c03e88bf7d5e3ed7d97bcd62ed
esmayk.tillesse <esmaykalves@gmail.com>
2018-11-10 22:40:34 -0300

    Criando operacao de instaciacao

--- src/main/java/br/com/one/OneApplication.java
+++ src/main/java/br/com/one/OneApplication.java
@@ -1,12 +1,30 @@
 package br.com.one;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.one.domain.Categoria;
+import br.com.one.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class OneApplication {
+public class OneApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 }
