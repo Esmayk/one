d168a3f9196aff43b5dce36606454edca7315c74
esmayk.tillesse <esmaykalves@gmail.com>
2018-11-15 15:48:26 -0300

    Ajuste nas categoria e criacao de class de tratamento de erros

--- src/main/java/br/com/one/services/CategoriaService.java
+++ src/main/java/br/com/one/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import br.com.one.domain.Categoria;
 import br.com.one.repositories.CategoriaRepository;
+import br.com.one.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
